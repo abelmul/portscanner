dc4089345ab2dc135889383faf1ba271a6277270
Abel Mulugeta <abelmulugeta2014@gmail.com>
2020-11-14 15:51:30 +0300

    remove unused code

--- portscanner.c
+++ portscanner.c
@@ -7,29 +7,6 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int getSO_ERROR(int fd) {
-    int err = 1;
-    socklen_t len = sizeof err;
-    if (-1 == getsockopt(fd, SOL_SOCKET, SO_ERROR, (char *)&err, &len)) {
-        perror("fatal getSO_ERROR");
-        exit(1);
-    }
-    if (err)
-        errno = err;              // set errno to the socket SO_ERROR
-    return err;
-}
-
-void closeSocket(int fd) {      // *not* the Windows closesocket()
-    if (fd >= 0) {
-        getSO_ERROR(fd); // first clear any errors, which can cause close to fail
-        if (shutdown(fd, SHUT_RDWR) < 0) // secondly, terminate the 'reliable' delivery
-            if (errno != ENOTCONN && errno != EINVAL) // SGI causes EINVAL
-                perror("shutdown");
-        if (close(fd) < 0) // finally call close()
-            perror("close");
-    }
-}
-
 int is_tcp_port_open(struct sockaddr_in* servaddr) {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
