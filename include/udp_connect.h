89cff84d88259b1d0fa53aa1f875cbcb1398ad2c
Abel Mulugeta <abelmulugeta2014@gmail.com>
2020-11-17 04:03:49 +0300

    untested udp scan

--- include/udp_connect.h
+++ include/udp_connect.h
@@ -6,18 +6,62 @@
  */
 void udp_cnct_scan(struct sockaddr_in* servaddr) {
     print_msg("Doing a UPD Connect Scan.");
+    char msg[] = "\xff\xffport scanner\x5f\x5f";
+    char msg2[4096];
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY; 
+    addr.sin_port = htons( 43593 ); 
+
     for(int i = 1; i < 65536; ++i){
         servaddr->sin_port   = htons(i);
+
+        int opt = 1;
         int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        int recvfd   = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
         if (sockfd == -1) {
             printf(RED"[Error] udp socket creation failed... for port %d, %s\n", ntohs(servaddr->sin_port), strerror(errno));
             exit(-1);
         }
 
-        if (connect(sockfd, (struct sockaddr*)servaddr, sizeof(*servaddr)) != 0) 
-            print_status(i, "open");
-        
+        if (recvfd == -1) {
+            printf(RED"[Error] icmp socket creation failed... for port %d, %s\n", ntohs(servaddr->sin_port), strerror(errno));
+            exit(-1);
+        }
 
+        Interrupter intr;
+        initInt &intr, recvfd, 3000);
+
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+        setsockopt(recvfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+        if ( bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) ||  bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) ) {
+            print_err2("Bind failed!", strerror(errno));
+            close(recvfd);
+            close(sockfd);
+            continue;
+        }
+
+        if(sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr*)servaddr, sizeof(*servaddr))  < 0) {
+            print_err2("Send failed ", strerror(errno));
+            close(recvfd);
+            close(sockfd);
+            continue;
+        }
+
+        pthread_t th;
+        if(pthread_create(&th, NULL, intr.stopListening, (void*)&intr) != 0) {
+            print_err2("Failed to create intre thread, ", strerror(errno));
+            exit(-1);
+        }
+
+        if (recvfrom(recvfd, msg2, sizeof(msg2), 0, (struct sockaddr*) servaddr, sizeof(*servaddr)) == 0) {
+        }
+
+
+        close(recvfd);
         close(sockfd);
     }
 }
