85f6f0d41e47ec208044608f03e55b74aa404b64
Abel Mulugeta <abelmulugeta2014@gmail.com>
2020-11-18 02:40:35 +0300

    udp scan according to nmap but can't fix segfault

--- include/udp_connect.h
+++ include/udp_connect.h
@@ -65,12 +65,15 @@ void udp_cnct_scan(struct sockaddr_in* servaddr) {
             exit(-1);
         }
 
+        // send three times
         if(sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr*)servaddr, sizeof(*servaddr))  < 0) {
             print_err2("Send failed ", strerror(errno));
             close(recvfd);
             close(sockfd);
             continue;
         }
+        sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr*)servaddr, sizeof(*servaddr));
+        sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr*)servaddr, sizeof(*servaddr));
 
 
         pthread_join(th, NULL);
@@ -94,16 +97,28 @@ void* recieve_icmp(void* ptr) {
         print_err2("Failed to create intre thread, ", strerror(errno));
         exit(-1);
     }
+    pthread_detach(th);
 
     if (recvfrom(args->recvfd, msg2, sizeof(msg2), 0, (struct sockaddr*) args->servaddr, &servlen) == 0) {
         // look at the icmp header here.
         struct icmphdr* icmph = (struct icmphdr*)(msg2 + sizeof(struct iphdr));
 
-        if (icmph->type != ICMP_UNREACH && icmph->code!=ICMP_UNREACH_PORT) {
-            print_status(args->port, "open");
+        if (icmph->type == ICMP_UNREACH) {
+            switch(icmph->code) {
+                case ICMP_UNREACH_PORT:
+                    // port is closed
+                    break;
+                case ICMP_UNREACH_HOST:
+                case ICMP_UNREACH_PROTOCOL:
+                case ICMP_UNREACH_NET_PROHIB:
+                case ICMP_UNREACH_HOST_PROHIB:
+                case ICMP_UNREACH_FILTER_PROHIB:
+                    print_status(args->port, "filtered");
+            }
         }
     }
 
     free(msg2);
 }
+
 #endif
